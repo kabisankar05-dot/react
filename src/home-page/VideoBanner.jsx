b148ce6b0e1b493baa23902b0a4193cd405fa8f2
kabisankar05-dot <kabisankar05@gmail.com>
2026-05-19 12:54:13 +0530

    video section

--- src/home-page/VideoBanner.jsx
+++ src/home-page/VideoBanner.jsx
@@ -1,26 +1,58 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Modal } from "react-bootstrap";
 import { FaPlay } from "react-icons/fa";
 import "../css/VideoBanner.css";
 
 const VideoBanner = () => {
+  const [showVideo, setShowVideo] = useState(false);
+
+  // YouTube Video Link
+  const videoUrl = "https://www.youtube.com/embed/330AoScKd5c";
+
   return (
-    <Container fluid className="video-section">
+    <>
+      <Container fluid className="video-section">
 
-      <div className="video-overlay"></div>
+        {/* Overlay */}
+        <div className="video-overlay"></div>
 
-      <div className="video-content">
-        <h2>
-          We’re the Best Organic <br />
-          Farm in the World
-        </h2>
+        {/* Content */}
+        <div className="video-content">
+          <h2>
+            We’re the Best Organic <br />
+            Farm in the World
+          </h2>
 
-        <button className="play-btn">
-          <FaPlay />
-        </button>
-      </div>
+          {/* Play Button */}
+          <button
+            className="play-btn"
+            onClick={() => setShowVideo(true)}
+          >
+            <FaPlay />
+          </button>
+        </div>
+      </Container>
 
-    </Container>
+      {/* Video Popup */}
+      <Modal
+        show={showVideo}
+        onHide={() => setShowVideo(false)}
+        centered
+        size="lg"
+      >
+        <Modal.Body className="video-modal-body">
+          <iframe
+            width="100%"
+            height="500"
+            src={videoUrl}
+            title="Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
