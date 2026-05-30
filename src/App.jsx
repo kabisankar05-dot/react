ee71e8587912ab6cecd552a5fea4bfa2cb4c0a03
kabisankar05-dot <kabisankar05@gmail.com>
2026-05-30 23:16:19 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,63 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./home-page/Header";
+import NavbarMenu from "./home-page/NavbarMenu";
+import CategorySidebar from "./home-page/Sidebar";
+import CarouselSection from "./home-page/Carousel";
+
 import Home from "./home-page/Home";
+// import Vegetables from "./pages/Vegetables";
+
 function App() {
+  const [showCategory, setShowCategory] = useState(true);
+
   return (
     <>
-      <Home />
+      <Header />
+
+      <Container>
+        <NavbarMenu
+          toggleCategory={() =>
+            setShowCategory(!showCategory)
+          }
+          showCategory={showCategory}
+        />
+
+        <Routes>
+          {/* Home Page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="mt-3">
+                  <Row>
+                    {showCategory && (
+                      <Col lg={3} md={4} className="d-none d-md-block">
+                        <CategorySidebar />
+                      </Col>
+                    )}
+
+                    <Col
+                      lg={showCategory ? 9 : 12}
+                      md={showCategory ? 8 : 12}
+                      sm={12}
+                    >
+                      <CarouselSection />
+                    </Col>
+                  </Row>
+                </div>
+
+                <Home />
+              </>
+            }
+          />
+
+          {/* Vegetables Page */}
+          {/* <Route path="/vegetables" element={<Vegetables />} /> */}
+        </Routes>
+      </Container>
     </>
   );
 }
--- src/home-page/Home.jsx
+++ src/home-page/Home.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import { Container } from "react-bootstrap";
 
-import Header from "./Header";
-import NavbarMenu from "./NavbarMenu";
-import CategorySidebar from "./Sidebar";
-import CarouselSection from "./Carousel";
 import ServiceSection from "./Service";
 import Banner from "./Banner";
 import Category from "./Category";
@@ -16,56 +12,10 @@ import BrandLogoSection from "./BrandLogoSection";
 import FooterSection from "./FooterSection";
 
 const Home = () => {
-
-  const [showCategory, setShowCategory] = useState(true);
-
-  const toggleCategory = () => {
-    setShowCategory(!showCategory);
-  };
-
   return (
     <>
-      {/* FULL WIDTH HEADER */}
-      <Header />
-
-      {/* CONTENT */}
       <Container>
-
-        {/* NAVBAR */}
-        <NavbarMenu
-          toggleCategory={() =>
-            setShowCategory(!showCategory)
-          }
-
-          showCategory={showCategory}
-        />
-
-        {/* HERO SECTION */}
-        <div className="mt-3">
-
-          <Row>
-
-            {/* LEFT CATEGORY */}
-            {showCategory && (
-              <Col lg={3} md={4} className="d-none d-md-block">
-                <CategorySidebar />
-              </Col>
-            )}
-
-            {/* RIGHT CAROUSEL */}
-            <Col
-              lg={showCategory ? 9 : 12}
-              md={showCategory ? 8 : 12}
-              sm={12}
-            >
-              <CarouselSection />
-            </Col>
-
-          </Row>
-
-        </div>
         <ServiceSection />
-
         <Banner />
         <Category />
         <FeaturedProducts />
@@ -74,9 +24,9 @@ const Home = () => {
         <TestimonialSection />
         <BrandLogoSection />
       </Container>
+
       <FooterSection />
     </>
-    
   );
 };
 
